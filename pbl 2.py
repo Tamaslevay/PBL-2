9ec475468235e76d2158bdfdc02a99fc2b35677b
Tamaslevay <110076623+Tamaslevay@users.noreply.github.com>
2022-08-03 13:17:38 +1000

    Created e^x taylor series

--- pbl 2.py
+++ pbl 2.py
@@ -11,3 +11,8 @@ def fact(n):
         output *= term
     return output
 
+def exptaylor(n,x):
+    output = 1
+    for term in range(1,n+1):
+        output += 1/fact(term)*x**term
+    return output
